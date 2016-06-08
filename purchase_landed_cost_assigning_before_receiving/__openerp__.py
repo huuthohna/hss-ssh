6c0c784dc403bedb32d2eaa0f11fbfe9af88d4d1
trananhdung <dungta2@hanelsoft.vn>
2016-06-08 12:00:04 +0700

    edit information

--- purchase_landed_cost_assigning_before_receiving/__openerp__.py
+++ purchase_landed_cost_assigning_before_receiving/__openerp__.py
@@ -1,20 +1,11 @@
 {
-    'name': 'Nippon Purchase - Landed Cost Extend',
-    'summary': '',
+    'name': 'Purchase Landed Cost - Assigning before Receiving',
+    'summary': 'Assigning Purchase Landed Cost for Incoming Shipment before receiving',
     'version': '1.0',
     'category': 'Purchase',
     'description': """
-    Make a new flow of purchase cost distribution.\n
-    - When make a Purchase Order, we have a checkbox to force waiting to
-     enter landed cost for this Purchase Order
-    - If you check Landed Cost box on PO form, the Incoming Shipment
-    that create by the Purchase Order will be Waiting for Landed Cost state
-    - In Purchase Cost Distribution form, we import Incoming Shipment in Waiting for
-    Landed Cost state and calculate landed cost for the Incoming Shipments.
-    - When you receipt Products by the Incoming Shipments, system will be calculate landed cost
-    for you and update cost into product form
     """,
-    'author': "HanelSoft ERP",
+    'author': "Hanel Software Solutions",
     'website': 'http://www.hanelsoft.vn/',
     'depends': ['purchase_landed_cost_posting_entries'],
     'data': ['views/views.xml',
